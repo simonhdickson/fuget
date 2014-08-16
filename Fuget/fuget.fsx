870519db46249f97cce3a0aab7b8a9f6295fb2a8
Simon Dickson <simonhdickson@gmail.com>
2014-08-16 08:55:40 +0100

    Only exposes single function when evaluated

--- Fuget/fuget.fsx
+++ Fuget/fuget.fsx
@@ -9,31 +9,31 @@ open System.Xml
 open System.Xml.Linq
 open System.Web
 
-let isUnix = Environment.OSVersion.Platform = PlatformID.Unix
-let isMacOS = Environment.OSVersion.Platform = PlatformID.MacOSX
-let isLinux = int System.Environment.OSVersion.Platform |> fun p -> (p = 4) || (p = 6) || (p = 128)
-let isMono = isLinux || isUnix || isMacOS
+let private isUnix = Environment.OSVersion.Platform = PlatformID.Unix
+let private isMacOS = Environment.OSVersion.Platform = PlatformID.MacOSX
+let private isLinux = int System.Environment.OSVersion.Platform |> fun p -> (p = 4) || (p = 6) || (p = 128)
+let private isMono = isLinux || isUnix || isMacOS
 
 type Version = Latest | Version of string
 
-let inline (++) left right = Path.Combine(left, right)
+let private (++) left right = Path.Combine(left, right)
 
-let downloadFile destination (url:Uri) =
+let private downloadFile destination (url:Uri) =
     use client = new WebClient()
     client.DownloadFile(url, destination)
 
-let downloadString (url:string) =
+let private downloadString (url:string) =
     use client = new WebClient()
     HttpUtility.UrlPathEncode url
     |> client.DownloadString
 
-let extract zip destination =
+let private extract zip destination =
     if isMono then
         let zipProcess = System.Diagnostics.Process.Start("unzip", sprintf" -d %s -q %s" destination zip)
         zipProcess.WaitForExit()
     else System.IO.Compression.ZipFile.ExtractToDirectory(zip, destination)
 
-let unpack name folder (url:string) =
+let private unpack name folder (url:string) =
     let destination = folder ++ name
     let nupkg = folder ++ (name + ".nupkg")
     if Directory.Exists destination then Directory.Delete (destination, true)
@@ -42,15 +42,15 @@ let unpack name folder (url:string) =
     extract nupkg destination
     destination
 
-let (|FolderExists|_|) lib root =
+let private (|FolderExists|_|) lib root =
     let directory = root ++ lib
     let files = lazy (Directory.EnumerateFiles(directory, "*.dll"))
     if Directory.Exists directory && Seq.length files.Value <> 0 then Some files.Value else None
 
-let toRelativePath (file:string) =
+let private toRelativePath (file:string) =
     file.Remove(0, __SOURCE_DIRECTORY__.Length + 1)
 
-let getLibs = function
+let private getLibs = function
     | FolderExists ((++)"lib" "net451") files
     | FolderExists ((++)"lib" "net45") files
     | FolderExists ((++)"lib" "net40") files
@@ -58,13 +58,13 @@ let getLibs = function
     | FolderExists "lib"                files -> files
     | _ -> failwith "unsupported nuget package"
 
-let printLibs libs =
+let private printLibs libs =
     printfn "Evaluate or add these to your script file:"
     libs
     |> Seq.map toRelativePath
     |> Seq.iter (printfn """#r @"%s" """)
 
-let getPackageUri xml =
+let private getPackageUri xml =
     let doc = XDocument.Parse xml in
         doc.Descendants().Elements()
         |> Seq.filter(fun  i -> i.Name.LocalName = "content")
@@ -72,7 +72,7 @@ let getPackageUri xml =
         |> Seq.map (fun i -> i.Value)
         |> Seq.exactlyOne
 
-let getPackage id = function
+let private getPackage id = function
     | Version version -> sprintf "https://www.nuget.org/api/v2/Packages(Id='%s',Version='%s')" id version
     | Latest          -> sprintf "https://www.nuget.org/api/v2/Packages()?$filter=Id eq '%s' and IsLatestVersion" id
 
